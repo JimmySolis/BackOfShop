a63f0a32446f47bf9f2f237e2d5ee365d65bbd03
Jimmy Solis <jimmysolis14@gmail.com>
2022-08-26 18:59:53 -0400

    I have made the category routes work.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,28 +2,52 @@ const router = require('express').Router();
 const { includes } = require('lodash');
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
-router.get('/', async (req, res) => res.json(await Category.findAll({ include: [{model : Product}]})));
-  // find all categories
-  // be sure to include its associated Products
-
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/', async (req, res) =>{
+   res.json(await Category.findAll({ include: [{model : Product}]}))
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+router.get('/:id', async (req, res) => { 
+  res.json(await Category.findByPk(req.params.id,{
+    include:[{ model: Product}]
+  }))
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.post('/', async (req, res) => {
+  try {
+    res.json(await Category.create(req.body))
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    res.json(await Category.update({
+      category_id : req.params.id,
+      category_name : req.body.category_name
+    },
+    {
+      where: {
+        category_id : req.params.id
+      }
+    }
+    ))
+  } catch (error) {
+    res.status(500).json({ message: `Could not updat Category ${req.body.category_id}`})
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+try {
+  res.json(await Category.destroy({ where: {
+    category_id : req.params.id
+  }
+}))
+} catch (error) {
+  res.status(500).json({message : `Could note delete Category ${req.params.id}`})
+}
 });
 
 module.exports = router;
