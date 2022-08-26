8fa77335d8883987ba5c73f4decc7bf940f02de5
Jimmy Solis <jimmysolis14@gmail.com>
2022-08-26 14:04:07 -0400

    I have made my env file. And have ajusted the connections file to support my credentials.

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,6 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(process.env.MYSQLURI);
 
 module.exports = sequelize;
